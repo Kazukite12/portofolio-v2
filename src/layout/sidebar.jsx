4e16236a811f8a773d294c5f32ad7ffac1b7eaf0
akmal abid <akmal.abid53@gmail.com>
2025-02-26 15:20:11 +0700

    fix responsive

--- src/layout/sidebar.jsx
+++ src/layout/sidebar.jsx
@@ -14,13 +14,14 @@ const Sidebar = () => {
   return (
     <div id="sidebar">
       <div className="sidebar-container">
-        <h1>AKMAl</h1>
-        <NavLink to="/" className="nav-link"><p>?</p> About</NavLink>
-        <NavLink to="/gallery" className="nav-link"><RiNftFill /> Project</NavLink>
-        <a href="/files/cv.pdf" download="cv.pdf"><GrDocumentDownload/> Resume</a>
+        <h1 className="normal-logo">AKMAL</h1>
+        <h1 className="mobile-logo">AL</h1>
+        <NavLink to="/" className="nav-link"><p>?</p> <p className="nav-text">About</p></NavLink>
+        <NavLink to="/gallery" className="nav-link"><RiNftFill /> <p className="nav-text">Project</p></NavLink>
+        <a href="/files/cv.pdf" download="cv.pdf"><GrDocumentDownload/> <p className="nav-text">Resume</p></a>
 
-        <a target='_blank' rel='noopener noreferrer' href="https://github.com/Kazukite12"><IoLogoGithub/> Github</a>
-        <a target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/muhammad-akmal-abid-56386723b/"><FaLinkedin/> Linkedin</a>
+        <a target='_blank' rel='noopener noreferrer' href="https://github.com/Kazukite12"><IoLogoGithub/> <p className="nav-text">Github</p></a>
+        <a target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/muhammad-akmal-abid-56386723b/"><FaLinkedin/> <p className="nav-text">Linkedin</p></a>
       
       </div>
     </div>
